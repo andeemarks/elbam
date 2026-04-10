e4ca8c6dc27aa48dde271940c9c71dacae75c2a8
Andy Marks <vampwillow@gmail.com>
2026-04-11 09:27:32 +1000

    Decomposed main processing

--- banking/__init__.py
+++ banking/__init__.py
@@ -5,11 +5,34 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def apply_transactions(balances: List[dict[str, str]], transactions: List[dict[str, str]]) -> List[dict[str, str]]:
-    new_balances = []
-    for balance in balances:
-        new_balances.append({'account_number': balance['account_number'], 'balance': float(balance['balance'])})
+def apply_transactions(balances: List[dict[str, str]], transactions: List[dict[str, str]]) -> List[dict[str, float]]:
+    new_balances = seed_balances(balances)
+    new_balances = record_transactions(balances, transactions, new_balances)
+    new_balances = sorted(new_balances, key=lambda b: b['account_number'])
 
+    logger.debug(pformat(new_balances))
+    closing_balances = aggregate_transactions_across_accounts(new_balances)
+
+    logger.debug(pformat(closing_balances))
+
+    return closing_balances
+
+
+def aggregate_transactions_across_accounts(new_balances: List[dict[str, float]]):
+    combined_new_balances: List[dict[str, float]] = []
+    for account_number, balance in groupby(new_balances, key=lambda b: b['account_number']):
+        # logger.debug(account_number)
+        # logger.debug(pformat(list(balance)))
+        total_balance = sum(b['balance'] for b in balance)
+        # logger.debug(total_balance)
+        combined_new_balances.append({'account_number': account_number, 'balance': total_balance})
+
+    return combined_new_balances
+
+
+def record_transactions(balances: List[dict[str, str]],
+                        transactions: List[dict[str, str]],
+                        new_balances: List[dict[str, float]]) -> List[dict[str, float]]:
     for transaction in transactions:
         from_account_number = transaction['from_account_number']
         to_account_number = transaction['to_account_number']
@@ -19,15 +42,12 @@ def apply_transactions(balances: List[dict[str, str]], transactions: List[dict[s
         new_balances.append({'account_number': from_account['account_number'], 'balance': -amount})
         new_balances.append({'account_number': to_account['account_number'], 'balance': amount})
 
-    new_balances = sorted(new_balances, key=lambda b: b['account_number'])
-    logger.debug(pformat(new_balances))
-    combined_new_balances = []
-    for account_number, balance in groupby(new_balances, key=lambda b: b['account_number']):
-        # logger.debug(account_number)
-        # logger.debug(pformat(list(balance)))
-        total_balance = sum(b['balance'] for b in balance)
-        # logger.debug(total_balance)
-        combined_new_balances.append({'account_number': account_number, 'balance': total_balance})
+    return new_balances
 
-    logger.debug(pformat(combined_new_balances))
-    return combined_new_balances
+
+def seed_balances(balances: List[dict[str, str]]) -> List[dict[str, float]]:
+    new_balances: List[dict[str, float]] = []
+    for balance in balances:
+        new_balances.append({'account_number': balance['account_number'], 'balance': float(balance['balance'])})
+
+    return new_balances
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,7 +1,6 @@
 import csv
 import logging
 logger = logging.getLogger(__name__)
-from pprint import pformat
 
 import banking
 
@@ -10,21 +9,17 @@ def test_full_transactions_happy_path():
         fieldnames = ['account_number', 'balance']
         reader = csv.DictReader(opening_balances_file, fieldnames=fieldnames, skipinitialspace=True, quoting=csv.QUOTE_NONNUMERIC)
         opening_balances = [row for row in reader]
-        logging.debug(pformat(opening_balances))
 
     with open("./tests/mable_account_balances_expected.csv") as closing_balances_file:
         fieldnames = ['account_number', 'balance']
         reader = csv.DictReader(closing_balances_file, fieldnames=fieldnames, skipinitialspace=True, quoting=csv.QUOTE_NONNUMERIC)
         closing_balances = [row for row in reader]
-        logging.debug(pformat(closing_balances))
 
     with open("./mable_transactions.csv") as tx_file:
         fieldnames = ['from_account_number', 'to_account_number', 'amount']
         reader = csv.DictReader(tx_file, fieldnames=fieldnames, skipinitialspace=True, quoting=csv.QUOTE_NONNUMERIC)
         transactions = [row for row in reader]
-        logging.debug(pformat(transactions))
 
     updated_balances = banking.apply_transactions(opening_balances, transactions) # type: ignore
-    logging.debug(pformat(updated_balances))
     
     assert sorted(updated_balances, key=lambda b: b['account_number']) == sorted(closing_balances, key=lambda b: b['account_number'])
