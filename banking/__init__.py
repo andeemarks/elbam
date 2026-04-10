6f727b67682a50b1e55b9a497553663ba1c0ef51
Andy Marks <vampwillow@gmail.com>
2026-04-10 15:51:47 +1000

    First passing test

---
+++ banking/__init__.py
@@ -0,0 +1,3 @@
+
+def apply_transactions(transactions: dict[str, str], balances: dict[str, str]) -> dict[str, str]:
+    return balances
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,2 +1,23 @@
+import csv
+import logging
+logger = logging.getLogger(__name__)
+from pprint import pformat
+
+import banking
+
 def test_full_transactions_happy_path():
-    assert True
+    with open("./mable_account_balances.csv") as balances_file:
+        fieldnames = ['account_number', 'balance']
+        reader = csv.DictReader(balances_file, fieldnames=fieldnames)
+        balances = [row for row in reader]
+        logging.info(pformat(balances))
+
+    with open("./mable_transactions.csv") as tx_file:
+        fieldnames = ['from_account_number', 'to_account_number', 'amount']
+        reader = csv.DictReader(tx_file, fieldnames=fieldnames)
+        transactions = [row for row in reader]
+        logging.info(pformat(transactions))
+
+    updated_balances = banking.apply_transactions(balances, transactions) # type: ignore
+    
+    assert updated_balances is not None
