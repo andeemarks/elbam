15c4729d150eb0eb40934e95279b8c181498d85b
Andy Marks <vampwillow@gmail.com>
2026-04-12 09:04:03 +1000

    Renaming for clarity

--- banking/__init__.py
+++ banking/__init__.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def apply_transactions(opening_accounts: List[dict[str, int]], transactions: List[dict[str, int]]) -> List[Account]:
-    processor = TransactionProcessor(raw_accounts=opening_accounts, raw_transactions=transactions)
+def apply_transactions(accounts: List[dict[str, int]], transactions: List[dict[str, int]]) -> List[Account]:
+    processor = TransactionProcessor(raw_accounts=accounts, raw_transactions=transactions)
 
     return processor.accounts
