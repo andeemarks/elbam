55e4197da20fb585bb834a0e59434ab7305e59fd
Andy Marks <vampwillow@gmail.com>
2026-04-11 11:21:55 +1000

    Moving aggregation behaviour into TransactionLog

--- banking/__init__.py
+++ banking/__init__.py
@@ -1,9 +1,8 @@
 from .account import Account
 from .transaction import Transaction
-from .transaction_log import TransactionLogEntry
+from .transaction_log import TransactionLogEntry, TransactionLog
 
 from pprint import pformat
-from itertools import groupby
 from typing import List
 import logging
 logger = logging.getLogger(__name__)
@@ -12,10 +11,9 @@ logger = logging.getLogger(__name__)
 def apply_transactions(balances: List[dict[str, str]], transactions: List[dict[str, str]]) -> List[Account]:
     opening_balances = seed_balances(balances)
     transaction_log = record_transactions(convert_transactions(transactions), opening_balances)
-    transaction_log = sorted(transaction_log, key=lambda account: account.account_number)
 
     # logger.info(pformat(transaction_log))
-    closing_balances = aggregate_transactions_across_accounts(transaction_log)
+    closing_balances = transaction_log.aggregate()
 
     logger.debug(pformat(closing_balances))
 
@@ -26,25 +24,14 @@ def convert_transactions(transactions: List[dict[str, str]]) -> List[Transaction
     return [Transaction(**transaction) for transaction in transactions]
 
 
-def aggregate_transactions_across_accounts(transaction_log: List[TransactionLogEntry]) -> List[Account]:
-    combined_new_balances: List[Account] = []
-    for account_number, balance in groupby(transaction_log, key=lambda account: account.account_number):
-        total_balance = sum(b.balance for b in balance)
-        combined_new_balances.append(Account(account_number, total_balance))
-
-    return combined_new_balances
-
-
 def record_transactions(transactions: List[Transaction],
-                        opening_balances: List[Account]) -> List[TransactionLogEntry]:
-    transaction_log: List[TransactionLogEntry] = []
+                        opening_balances: List[Account]) -> TransactionLog:
+    transaction_log: TransactionLog = TransactionLog()
     for balance in opening_balances:
-        transaction_log.append(TransactionLogEntry(balance.account_number, balance.balance))
+        transaction_log.add_log_entry(TransactionLogEntry(balance.account_number, balance.balance))
 
     for transaction in transactions:
-        (debit, credit) = TransactionLogEntry.from_transaction(transaction)
-        transaction_log.append(debit)
-        transaction_log.append(credit)
+        transaction_log.add_transaction(transaction)
 
     return transaction_log
 
--- banking/transaction_log.py
+++ banking/transaction_log.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from itertools import groupby
 from .transaction import Transaction
+from .account import Account
 
 
 @dataclass
@@ -7,7 +9,27 @@ class TransactionLogEntry:
     account_number: str
     balance: float
 
-    @classmethod
-    def from_transaction(cls, transaction: Transaction) -> tuple[TransactionLogEntry, TransactionLogEntry]:
-        return (TransactionLogEntry(transaction.from_account_number, -transaction.amount),
-                TransactionLogEntry(transaction.to_account_number, transaction.amount))
+
+@dataclass
+class TransactionLog:
+    log: list[TransactionLogEntry] = field(default_factory=list)
+
+    def add_log_entry(self, log_entry: TransactionLogEntry):
+        self.log.append(log_entry)
+
+    def add_transaction(self, transaction: Transaction):
+        self.log.append(TransactionLogEntry(transaction.from_account_number, -transaction.amount))
+        self.log.append(TransactionLogEntry(transaction.to_account_number, transaction.amount))
+
+    def sorted(self) -> TransactionLog:
+        sorted_log = sorted(self.log, key=lambda log_entry: log_entry.account_number)
+
+        return TransactionLog(log=sorted_log)
+
+    def aggregate(self) -> list[Account]:
+        combined_new_balances: list[Account] = []
+        for account_number, balance in groupby(self.sorted().log, key=lambda account: account.account_number):
+            total_balance = sum(b.balance for b in balance)
+            combined_new_balances.append(Account(account_number, total_balance))
+
+        return combined_new_balances
